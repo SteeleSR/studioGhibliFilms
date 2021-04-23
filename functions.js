345cf9130dda92b95d0f77cca7286aefd405eca6
SteeleSR <samuelrsteele@gmail.com>
2021-04-23 14:50:16 +0100

    created intro, main screen and populated with assets and function to move between screens

---
+++ functions.js
@@ -0,0 +1,59 @@
+const enterMainScreen = () => {
+    document.getElementById('introductionScreen').style.display = 'none'
+    document.getElementById('mainScreen').style.display = 'block'
+    document.body.style.background = '#638ECB';
+}
+
+const enterSpiritedScreen = () => {
+    document.getElementById('mainScreen').style.display = 'none'
+    document.getElementById('spiritedScreen').style.display = 'block'
+}
+
+// const getSpiritedData = () => {
+//     fetch()
+// }
+
+const enterMononokeScreen = () => {
+    document.getElementById('mainScreen').style.display = 'none'
+    document.getElementById('mononokeScreen').style.display = 'block'
+}
+
+// const getMononokeData = () => {
+//
+// }
+
+const enterCastleScreen = () => {
+    document.getElementById('mainScreen').style.display = 'none'
+    document.getElementById('castleScreen').style.display = 'block'
+}
+
+// const getCastleData = () => {
+//
+// }
+
+const enterPonyoScreen = () => {
+    document.getElementById('mainScreen').style.display = 'none'
+    document.getElementById('ponyoScreen').style.display = 'block'
+}
+
+// const getPonyoData = () => {
+//
+// }
+
+const enterHowlsScreen = () => {
+    document.getElementById('mainScreen').style.display = 'none'
+    document.getElementById('howlsScreen').style.display = 'block'
+}
+
+// const getHowlsData = () => {
+//
+// }
+
+const enterTotoroScreen = () => {
+    document.getElementById('mainScreen').style.display = 'none'
+    document.getElementById('totoroScreen').style.display = 'block'
+}
+//
+// const getTotoroData = () => {
+//
+// }
---
+++ index.js
@@ -0,0 +1,27 @@
+document.getElementById('introductionButton').addEventListener('click', () => {
+    enterMainScreen()
+})
+
+document.getElementById('spiritedButton').addEventListener('click', () => {
+    enterSpiritedScreen()
+})
+
+document.getElementById('princessButton').addEventListener('click', () => {
+    enterMononokeScreen()
+})
+
+document.getElementById('castleButton').addEventListener('click', () => {
+    enterCastleScreen()
+})
+
+document.getElementById('ponyoButton').addEventListener('click', () => {
+    enterPonyoScreen()
+})
+
+document.getElementById('howlsButton').addEventListener('click', () => {
+    enterHowlsScreen()
+})
+
+document.getElementById('totoroButton').addEventListener('click', () => {
+    enterTotoroScreen()
+})
